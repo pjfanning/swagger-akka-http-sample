bf4c53930b1f0421bdbcd1982caecb968e8647b3
PJ Fanning <pj.fanning@workday.com>
2016-07-16 00:55:10 +0100

    Support security definitions

--- src/main/scala/com/example/akka/swagger/SwaggerDocService.scala
+++ src/main/scala/com/example/akka/swagger/SwaggerDocService.scala
@@ -7,6 +7,8 @@ import com.github.swagger.akka._
 import com.github.swagger.akka.model.`package`.Info
 import com.example.akka.add.AddService
 import com.example.akka.hello.HelloService
+import io.swagger.models.ExternalDocs
+import io.swagger.models.auth.BasicAuthDefinition
 
 class SwaggerDocService(system: ActorSystem) extends SwaggerHttpService with HasActorSystem {
   override implicit val actorSystem: ActorSystem = system
@@ -14,4 +16,6 @@ class SwaggerDocService(system: ActorSystem) extends SwaggerHttpService with Has
   override val apiTypes = Seq(ru.typeOf[AddService], ru.typeOf[HelloService])
   override val host = "localhost:12345"
   override val info = Info(version = "1.0")
+  override val externalDocs = Some(new ExternalDocs("Core Docs", "http://acme.com/docs"))
+  override val securitySchemeDefinitions = Map("basicAuth" -> new BasicAuthDefinition())
 }
