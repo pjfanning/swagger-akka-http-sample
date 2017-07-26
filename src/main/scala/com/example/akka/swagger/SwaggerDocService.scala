b6f78466157e3838f250597cb6aadbf284bf5417
pj.fanning <pj.fanning@workday.com>
2017-07-26 13:24:35 +0100

    Further changes due to incompatible changes in swagger-akka-http 0.10.0-SNAPSHOT

--- src/main/scala/com/example/akka/swagger/SwaggerDocService.scala
+++ src/main/scala/com/example/akka/swagger/SwaggerDocService.scala
@@ -1,6 +1,5 @@
 package com.example.akka.swagger
 
-import scala.reflect.runtime.{universe => ru}
 import com.github.swagger.akka.SwaggerHttpService
 import com.github.swagger.akka.model.Info
 import com.example.akka.add.AddService
@@ -9,9 +8,10 @@ import io.swagger.models.ExternalDocs
 import io.swagger.models.auth.BasicAuthDefinition
 
 object SwaggerDocService extends SwaggerHttpService {
-  override val apiTypes = Seq(ru.typeOf[AddService], ru.typeOf[HelloService])
+  override val apiClasses = Set(classOf[AddService], classOf[HelloService])
   override val host = "localhost:12345"
   override val info = Info(version = "1.0")
   override val externalDocs = Some(new ExternalDocs("Core Docs", "http://acme.com/docs"))
   override val securitySchemeDefinitions = Map("basicAuth" -> new BasicAuthDefinition())
+  override val unwantedDefinitions = Seq("Function1", "Function1RequestContextFutureRouteResult")
 }
