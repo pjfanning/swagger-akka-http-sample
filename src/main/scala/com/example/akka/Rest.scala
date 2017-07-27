0969ac4f71dd35e0b4f74cf0b2bb93f83f369dcf
pj.fanning <pj.fanning@workday.com>
2017-07-27 14:56:08 +0100

    Remove cake patten from `Rest` class

--- src/main/scala/com/example/akka/Rest.scala
+++ src/main/scala/com/example/akka/Rest.scala
@@ -1,3 +1,26 @@
 package com.example.akka
 
-object Rest extends App with BootedCore with CoreActors with Api with Web
+import akka.actor.{ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.RouteConcatenation
+import akka.stream.ActorMaterializer
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
+import com.example.akka.add.{AddActor, AddService}
+import com.example.akka.hello.{HelloActor, HelloService}
+import com.example.akka.swagger.SwaggerDocService
+
+object Rest extends App with RouteConcatenation {
+  implicit val system = ActorSystem("akka-http-sample")
+  sys.addShutdownHook(system.terminate())
+
+  implicit val materializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+
+  val add = system.actorOf(Props[AddActor])
+  val hello = system.actorOf(Props[HelloActor])
+  val routes =
+    cors() (new AddService(add).route ~
+      new HelloService(hello).route ~
+      SwaggerDocService.routes)
+  Http().bindAndHandle(routes, "0.0.0.0", 12345)
+}
