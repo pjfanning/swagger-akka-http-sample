49031deb4bfac8dbc55fb7f7d048095817c41f62
PJ Fanning <fanningpj@yahoo.com>
2018-10-07 00:43:39 +0100

    fix issue with Option[Int] schema annotation

--- src/main/scala/com/example/akka/addoption/AddOptionActor.scala
+++ src/main/scala/com/example/akka/addoption/AddOptionActor.scala
@@ -3,13 +3,11 @@ package com.example.akka.addoption
 import akka.actor.Actor
 import io.swagger.v3.oas.annotations.media.Schema
 
-import scala.annotation.meta.field
-
 object AddOptionActor {
   // due to type erasure of inner types for scala.Option[T] when T is a primitive type like Int,
   // the only way to ensure generated swagger doc has int type for number2 is to use an annotation
   case class AddOptionRequest(number: Int,
-                              @Schema(required = false, `type` = "int") number2: Option[Int] = None)
+                              @Schema(required = false, implementation = classOf[Int]) number2: Option[Int] = None)
   case class AddOptionResponse(sum: Int)
 }
 
