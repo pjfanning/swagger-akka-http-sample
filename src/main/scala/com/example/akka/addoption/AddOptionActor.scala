8ea45fd869b21c6e36bbcf833f5cda5c661d791e
PJ Fanning <pj@kontainers.co.uk>
2020-03-09 13:42:14 +0100

    optional boolean

--- src/main/scala/com/example/akka/addoption/AddOptionActor.scala
+++ src/main/scala/com/example/akka/addoption/AddOptionActor.scala
@@ -4,10 +4,10 @@ import akka.actor.Actor
 import io.swagger.v3.oas.annotations.media.Schema
 
 object AddOptionActor {
-  // due to type erasure of inner types for scala.Option[T] when T is a primitive type like Int,
-  // the only way to ensure generated swagger doc has int type for number2 is to use an annotation
-  case class AddOptionRequest(number: Int,
-                              @Schema(required = false, implementation = classOf[Int]) number2: Option[Int] = None)
+  // due to type erasure of inner types for scala.Option[T] when T is a primitive type like Int or Boolean,
+  // the only way to ensure generated swagger doc has int/boolean type for number2 is to use an annotation
+  case class AddOptionRequest(number: Boolean,
+                              @Schema(required = false, implementation = classOf[Boolean]) number2: Option[Boolean] = None)
   case class AddOptionResponse(sum: Int)
 }
 
@@ -16,7 +16,9 @@ class AddOptionActor extends Actor {
 
   def receive: Receive = {
     case request: AddOptionRequest => {
-      sender ! AddOptionResponse(request.number + request.number2.getOrElse(0))
+      sender ! AddOptionResponse(booleanToInt(request.number) + request.number2.map(booleanToInt).getOrElse(0))
     }
   }
+
+  private def booleanToInt(b: Boolean) = if (b) 1 else 0
 }
