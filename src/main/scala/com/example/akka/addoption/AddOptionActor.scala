7abeb2bd2a014606e6352702ce9eeb2b1c567793
PJ Fanning <pj@kontainers.co.uk>
2020-03-17 16:34:51 +0100

    optional boolean example

--- src/main/scala/com/example/akka/addoption/AddOptionActor.scala
+++ src/main/scala/com/example/akka/addoption/AddOptionActor.scala
@@ -8,8 +8,8 @@ import scala.annotation.meta.field
 object AddOptionActor {
   // due to type erasure of inner types for scala.Option[T] when T is a primitive type like Int,
   // the only way to ensure generated swagger doc has int type for number2 is to use an annotation
-  case class AddOptionRequest(number: Int,
-                              @(ApiModelProperty @field)(dataType = "int") number2: Option[Int] = None)
+  case class AddOptionRequest(number: Boolean,
+                              @(ApiModelProperty @field)(dataType = "boolean") number2: Option[Boolean] = None)
   case class AddOptionResponse(sum: Int)
 }
 
@@ -18,7 +18,9 @@ class AddOptionActor extends Actor {
 
   def receive: Receive = {
     case request: AddOptionRequest => {
-      sender ! AddOptionResponse(request.number + request.number2.getOrElse(0))
+      sender ! AddOptionResponse(booleanToInt(request.number) + booleanToInt(request.number2.getOrElse(false)))
     }
   }
+
+  private def booleanToInt(b: Boolean) = if (b) 1 else 0
 }
