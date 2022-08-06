2ce75d70924c72c5a0c89b26e7faef655429ce08
PJ Fanning <pjfanning@users.noreply.github.com>
2022-08-06 11:12:08 +0100

    upgrade to swagger-scala-module 2.7.0

--- src/main/scala/com/example/akka/addoption/AddOptionActor.scala
+++ src/main/scala/com/example/akka/addoption/AddOptionActor.scala
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.media.Schema
 object AddOptionActor {
   // due to type erasure of inner types for scala.Option[T] when T is a primitive type like Int,
   // the only way to ensure generated swagger doc has int type for number2 is to use an annotation
+  // if you upgrade to swagger-scala-module v2.7.0, the Schema annotation can be removed (but is still needed for Scala 3 users)
   case class AddOptionRequest(number: Int,
                               @Schema(required = false, implementation = classOf[Int]) number2: Option[Int] = None)
+  //case class AddOptionRequest(number: Int, number2: Option[Int] = None)
   case class AddOptionResponse(sum: Int)
 }
 
